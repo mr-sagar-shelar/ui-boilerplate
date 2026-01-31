6d75859fb49fcccadf6643d7c9a84c22ebf34be7
mr-sagar-shelar <shaggy.shelar@gmail.com>
2026-01-31 19:16:24 +0530

    Added vitest with example component

--- apps/web-example/vite.config.ts
+++ apps/web-example/vite.config.ts
@@ -1,7 +1,21 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { playwright } from '@vitest/browser-playwright'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    dedupe: ['react', 'react-dom'],
+  },
+  test: {
+    browser: {
+      enabled: true,
+      provider: playwright(),
+      instances: [
+        { browser: 'chromium' },
+      ],
+    },
+  },
 })
